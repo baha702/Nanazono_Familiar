a195bdf2cef0a4d86ff9a9f098b1fc7b6a3d20cc
Asa005 <m011900517@edu.teu.ac.jp>
2020-12-07 14:15:03 +0900

    基本動作！

---
+++ Nanazono_Familiar/Assets/Player.cs
@@ -0,0 +1,36 @@
+﻿// Player.cs
+using UnityEngine;
+
+// プレイヤー
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private Vector3 velocity;              // 移動方向
+    [SerializeField] private float moveSpeed = 5.0f;        // 移動速度
+
+    void Update()
+    {
+        // WASD
+        velocity = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+            velocity.z += 1;
+        if (Input.GetKey(KeyCode.A))
+            velocity.x -= 1;
+        if (Input.GetKey(KeyCode.S))
+            velocity.z -= 1;
+        if (Input.GetKey(KeyCode.D))
+            velocity.x += 1;
+
+        // 速度ベクトルの長さを1秒でmoveSpeedだけ進める
+        velocity = velocity.normalized * moveSpeed * Time.deltaTime;
+
+        // いずれかの方向に移動している場合
+        if (velocity.magnitude > 0)
+        {
+            // プレイヤーの位置(transform.position)の更新
+            // 移動方向ベクトル(velocity)の追加
+            transform.position += velocity;
+        }
+
+    }
+}
