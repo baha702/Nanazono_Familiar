41bc0478545c99a5ad547213e141fb7f3a3c5830
Hosaka0712 <m011925549@edu.teu.ac.jp>
2021-03-27 10:42:04 +0900

    ネプリーグぽいステージとエネミーを作りました

--- Nanazono_Familiar/Assets/PlayerHPBar.cs
+++ Nanazono_Familiar/Assets/PlayerHPBar.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class PlayerHPBar : MonoBehaviour
 {
     //最大HPと現在のHP。
-    int maxHp = 5;
+    int maxHp = 1;
     int currentHp;
     //Sliderを入れる
     public Slider slider;
