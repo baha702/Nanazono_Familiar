e449fc85e419d62b6d8001b0a9885bc6282c4797
m0119285 <m011928523@edu.teu.ac.jp>
2021-03-31 17:08:13 +0900

    プロトタイプ二号

---
+++ Nanazono_Familiar/Assets/Script/FiveJudge.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiveJudge : MonoBehaviour
+{
+        public GameObject enemyName1,enemyName3,enemyName4,enemyName5;
+        public string enemystr1,enemystr3,enemystr4,enemystr5;
+        public GameObject enemyName2;
+        public string enemystr2;
+        public int strLength;
+        private int strFlag;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            /*if (Destroyflag1==true && Destroyflag2==true)
+            {
+                StartCoroutine("Coroutine");
+            }*/
+            if (strFlag >= strLength)
+            {
+                StartCoroutine("Coroutine");
+            }
+        }
+
+
+        private IEnumerator Coroutine()
+        {
+            //１秒待機
+            yield return new WaitForSeconds(2.0f);
+
+            Destroy(this.gameObject);
+
+            //コルーチンを終了
+            yield break;
+        }
+
+        private void JudgeName(string str1, GameObject enemyObject)
+        {
+            if (GameObject.Find("FlyingText") != null)
+            {
+                GameObject JudgeText = GameObject.Find("FlyingText");
+                if (GameObject.Find(str1) != null)
+                {
+                    GameObject childA = JudgeText.transform.Find(str1).gameObject;//FlyingTextの子の「ト」という名前のオブジェクトを探して変数childAに入れる
+                    childA.GetComponent<BoxCollider>().isTrigger = true;
+                    Debug.Log(str1);
+                    Destroy(enemyObject);
+                    //enemyObject.gameObject.SetActive(true);
+                    strFlag++;
+                }
+            }
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            JudgeName(enemystr1, enemyName1);
+            JudgeName(enemystr2, enemyName2);
+            JudgeName(enemystr3, enemyName3);
+            JudgeName(enemystr4, enemyName4);
+            JudgeName(enemystr5, enemyName5);
+
+
+        }
+    }
--- Nanazono_Familiar/Assets/Script/ThreeJudge.cs
+++ Nanazono_Familiar/Assets/Script/ThreeJudge.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NameJudge : MonoBehaviour
+public class ThreeJudge : MonoBehaviour
 {
-    public GameObject enemyName1;
-    public string enemystr1;     
+    public GameObject enemyName1, enemyName3;
+    public string enemystr1, enemystr3;
     public GameObject enemyName2;
     public string enemystr2;
     public int strLength;
@@ -14,7 +14,7 @@ public class NameJudge : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -42,7 +42,7 @@ public class NameJudge : MonoBehaviour
         yield break;
     }
 
-    private void JudgeName(string str1,GameObject enemyObject)
+    private void JudgeName(string str1, GameObject enemyObject)
     {
         if (GameObject.Find("FlyingText") != null)
         {
@@ -61,32 +61,12 @@ public class NameJudge : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        JudgeName(enemystr1,enemyName1);
-        JudgeName(enemystr2,enemyName2);
+        JudgeName(enemystr1, enemyName1);
+        JudgeName(enemystr2, enemyName2);
+        JudgeName(enemystr3, enemyName3);
+       
 
-        /*if (GameObject.Find("FlyingText") != null)
-        {
-            GameObject JudgeText = GameObject.Find("FlyingText");
-            if (GameObject.Find("ト") != null)
-            {
-                GameObject childA = JudgeText.transform.Find("ト").gameObject;//FlyingTextの子の「ト」という名前のオブジェクトを探して変数childAに入れる
-                childA.GetComponent<BoxCollider>().isTrigger = true;
-                Debug.Log("ト");
-                enemyName1.gameObject.SetActive(true);
-                Destroyflag1 = true;
-            }
-        }
-        if (GameObject.Find("FlyingText") != null)
-        {
-            GameObject JudgeText = GameObject.Find("FlyingText");
-            if (GameObject.Find("ム") != null)
-            {
-                GameObject childB = JudgeText.transform.Find("ム").gameObject;//FlyingTextの子の「ト」という名前のオブジェクトを探して変数childAに入れる
-                childB.GetComponent<BoxCollider>().isTrigger = true;
-                Debug.Log("ム！！！");
-                enemyName2.gameObject.SetActive(true);
-                Destroyflag2 = true;
-            }
-        }*/
+
     }
 }
+
