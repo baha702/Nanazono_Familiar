e597b8fbed297a195f06c9e8bcca5b2000dfe412
m0119285 <m011928523@edu.teu.ac.jp>
2021-05-11 21:51:05 +0900

    敵エネミーの名前を判定するスクリプトをまとめました。エフェクトの色とスピードを変えてます。敵が湧かなくなるエラーを直しました。

---
+++ Nanazono_Familiar/Assets/Script/EnemyScript/EnemyMoveAI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMoveAI : MonoBehaviour
+{
+    // 目的地となるGameObjectをセットします。
+    public GameObject target;
+    private NavMeshAgent myAgent;
+
+    void Start()
+    {
+        // Nav Mesh Agent を取得します。
+        myAgent = GetComponent<NavMeshAgent>();
+        target = GameObject.Find("Target");
+    }
+
+    void Update()
+    {
+        // targetに向かって移動します。
+        myAgent.SetDestination(target.transform.position);
+    }
+}
---
+++ Nanazono_Familiar/Assets/Script/NameScript/NameJudge.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NameJudge : MonoBehaviour
+{
+    [SerializeField] int strLength;
+    [SerializeField] string[] enemyStr;
+    [SerializeField] TextMeshProUGUI[] enemyTMP;
+
+    private int strFlag;
+    public Material blueMaterial,redMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (strFlag >= strLength)
+        {
+            StartCoroutine("Coroutine");
+        }
+    }
+
+
+    private IEnumerator Coroutine()
+    {
+        //１秒待機
+        yield return new WaitForSeconds(2.0f);
+
+        this.gameObject.SetActive(false);
+        for (int i = 0; i < strLength; i++)
+        {
+            enemyTMP[i].fontSharedMaterial = redMaterial;
+        }
+        strFlag = 0;
+        Destroy(this.gameObject);
+
+        //コルーチンを終了
+        yield break;
+    }
+   
+    private void NameJudges(string str,TextMeshProUGUI TMPstr)
+    {
+        
+        if (GameObject.Find("FlyingText") != null)
+        {
+            GameObject JudgeText = GameObject.Find("FlyingText");
+            if (GameObject.Find(str) != null)
+            {
+                TMPstr.text = string.Format(str);
+                TMPstr.fontSharedMaterial = blueMaterial;
+                strFlag++;
+
+            }
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        for (int i = 0; i<strLength; i++)
+        {
+            NameJudges(enemyStr[i],enemyTMP[i]);
+        }
+
+        if (GameObject.Find("Target"))
+        {
+            StartCoroutine("Coroutine");
+        }
+    }
+}
