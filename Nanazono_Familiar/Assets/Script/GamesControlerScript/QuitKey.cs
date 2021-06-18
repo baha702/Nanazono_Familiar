8386ab9d26e375a4ed3402855db97512edf393f9
Hosaka0712 <m011925549@edu.teu.ac.jp>
2021-06-18 22:23:31 +0900

    各シーンにゲーム終了キーを追加しました

---
+++ Nanazono_Familiar/Assets/Script/GamesControlerScript/QuitKey.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitKey : MonoBehaviour
+{
+    private AudioSource audio;
+    public AudioClip QuitClip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+    void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+      UnityEngine.Application.Quit();
+#endif
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape)) Quit();
+        //audio.PlayOneShot(titleClip, 1.0f);
+    }
+}
