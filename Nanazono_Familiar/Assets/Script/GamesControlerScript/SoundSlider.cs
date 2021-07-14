4842b008a60e24c99034ae2bcc15488dcc2c8fc8
Hosaka0712 <m011925549@edu.teu.ac.jp>
2021-07-14 18:57:36 +0900

    音量調整とビルドした際にウィンドウの大きさを変えられるようにしました

---
+++ Nanazono_Familiar/Assets/Script/GamesControlerScript/SoundSlider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;//Sliderを使用するために必要
+
+[RequireComponent(typeof(Slider))]
+public class SoundSlider : MonoBehaviour
+{
+
+    Slider m_Slider;//音量調整用スライダー
+
+   
+    [SerializeField]
+    float m_ScroolSpeed = 1;//キー入力で調整バーを動かすスピード
+    void Awake()
+    {
+        m_Slider = GetComponent<Slider>();
+        m_Slider.value = AudioListener.volume;
+    }
+
+    private void OnEnable()
+    {
+        m_Slider.value = AudioListener.volume;
+        //スライダーの値が変更されたら音量も変更する
+        m_Slider.onValueChanged.AddListener((sliderValue) => AudioListener.volume = sliderValue);
+    }
+
+    private void OnDisable()
+    {
+        m_Slider.onValueChanged.RemoveAllListeners();
+    }
+}
