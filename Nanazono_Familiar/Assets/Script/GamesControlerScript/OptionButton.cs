ce76a6f36fa6491668655b07de387f490fa3c85c
Hosaka0712 <m011925549@edu.teu.ac.jp>
2021-08-19 21:37:33 +0900

    オプションなどをいじりました

---
+++ Nanazono_Familiar/Assets/Script/GamesControlerScript/OptionButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionButton : MonoBehaviour
+{
+    private AudioSource audio;
+    public AudioClip startClip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    public void StartGame()
+    {
+        //audio.PlayOneShot(startClip, 1.0f);
+        SceneManager.LoadScene("GameOption");
+    }
+}
