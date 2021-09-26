715506a249b18ea0c34f3ca1e45e1f2af99f6529
Hosaka0712 <m011925549@edu.teu.ac.jp>
2021-09-26 15:14:15 +0900

    リンゴのマテリアルを入れ替えたのと、デモボンが死んだとき雑魚も死ぬようにしました

--- Nanazono_Familiar/Assets/Script/NameScript/NewNameJudgeBoss.cs
+++ Nanazono_Familiar/Assets/Script/NameScript/NewNameJudgeBoss.cs
@@ -99,10 +99,10 @@ public class NewNameJudgeBoss : MonoBehaviour
 
         atombool = false;
 
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(0.5f);
         SceneManager.LoadScene("GameClear");
-        this.gameObject.SetActive(false);
         Destroy(this.gameObject);
+        this.gameObject.SetActive(false);      
         strFlag = 0;
         //コルーチンを終了
         yield break;
