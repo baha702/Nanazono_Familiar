3dd436e4f742fa77c15b48c834ef451a0c7d37e7
Hosaka0712 <m011925549@edu.teu.ac.jp>
2021-09-29 21:51:21 +0900

    エンドロールを作成しました

---
+++ Nanazono_Familiar/Assets/Script/GamesControlerScript/Endroll.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Endroll : MonoBehaviour
+{
+    //　テキストのスクロールスピード
+    [SerializeField]
+    private float textScrollSpeed = 30;
+    //　テキストの制限位置
+    [SerializeField]
+    private float limitPosition = 730f;
+    //　エンドロールが終了したかどうか
+    private bool isStopEndRoll;
+    //　シーン移動用コルーチン
+    private Coroutine endRollCoroutine;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //　エンドロールが終了した時
+        if (isStopEndRoll)
+        {
+            endRollCoroutine = StartCoroutine(GoToNextScene());
+        }
+        else
+        {
+            //　エンドロール用テキストがリミットを越えるまで動かす
+            if (transform.position.y <= limitPosition)
+            {
+                transform.position = new Vector2(transform.position.x, transform.position.y + textScrollSpeed * Time.deltaTime);
+            }
+            else
+            {
+                isStopEndRoll = true;
+            }
+        }
+    }
+
+    IEnumerator GoToNextScene()
+    {
+        //　5秒間待つ
+        yield return new WaitForSeconds(2f);       
+            StopCoroutine(endRollCoroutine);
+            SceneManager.LoadScene("Start Voice");
+        yield return null;
+    }
+}
